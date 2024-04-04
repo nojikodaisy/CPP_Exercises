21ca80a7578d3a7305fd361143d2a5486867229f
Sinem ARIK <sinem.arik@edu.univ-eiffel.fr>
2024-04-04 12:44:55 +0200

    tp10 fin séance

--- tp10/src/midgray.cpp
+++ tp10/src/midgray.cpp
@@ -5,10 +5,10 @@
 
 int main()
 {
-    // const auto image = ...
-    // for (size_t i = 0; i < 800; ++i)
-    //     for (size_t j = 0; j < 600; ++j)
-    //         assert(image(i, j) == 128u);
-    // image_lib::save(image, "images/midgray.png"); // Ouvrez moi !!
+    const auto image = Image<uint8_t, 800, 600> { 128 };
+    for (size_t i = 0; i < 800; ++i)
+        for (size_t j = 0; j < 600; ++j)
+            assert(image(i, j) == 128u);
+    image_lib::save(image, "images/midgray.png"); // Ouvrez moi !!
     return 0;
 }
